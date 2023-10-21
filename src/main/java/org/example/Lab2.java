ed7d0a0ef7a411b9583c650950a76fc5ba851e88
Hajrudin <hajrudin.bajraktarevic@stu.ibu.edu.ba>
2023-10-22 00:08:51 +0200

    adding files

--- src/main/java/org/example/Lab2.java
+++ src/main/java/org/example/Lab2.java
@@ -39,6 +39,7 @@ public class Lab2 {
         //3.
         int sum1=0;
         while(true){
+            System.out.println("Enter a Number to sum up:");
             int read1 = Integer.parseInt(reader.nextLine());
             if(read1==0){
                 break;
@@ -50,9 +51,116 @@ public class Lab2 {
         }
         System.out.println("Sum in the End is: "+sum1);
         //4.
+        System.out.println("Enter the first Number: ");
+        int firstNumber =Integer.parseInt(reader.nextLine());
+        System.out.println("Enter the last Number: ");
+        int lastNumber =Integer.parseInt(reader.nextLine());
+        System.out.println("First: "+firstNumber);
+        System.out.println("Last: "+lastNumber);
+        while(true){
+        if(!(firstNumber>lastNumber)){
+            System.out.println(firstNumber);
+            firstNumber++;
+
+        }else{
+            break;
+        }
+        }
+        //5.
+        System.out.println("Enter a Number: ");
+        int i = Integer.parseInt(reader.nextLine());
+        int result = (int)Math.pow(2,i);
+        System.out.println("The result is: " + result);
+        //6.
+        printText();
+        System.out.println("How many times do u want the Text above to be printed?");
+        int textPrint = Integer.parseInt(reader.nextLine());
+        int n=0;
+        while(n<textPrint){
+            printText();
+            n++;
+        }
+        //7.
+        printStars(5);
+        printStars(3);
+        printStars(9);
+        //8.
+        drawStarsPiramid(5);
+        drawStarsPiramid(10);
+        //8.2
+        drawInvertedStarsPiramid(5);
+        drawInvertedStarsPiramid(10);
+        //9.
 
 
     }
     //methods here
+    //6.
+    public static void printText() {
+        System.out.println("In the beginning there were the swamp, the hoe and Java.");
+    }
+    //7.
+    public static void printStars(int amount){
+         int m =0;
+        while(m<amount) {
+            System.out.print("*");
+            m++;
+        }
+        System.out.println();
+    }
+    public static void drawStarsPiramid(int rows){
+        int n=1;
+        while(n<=rows){
+            int j =1;
+            while(j<=n){
+                System.out.print("* ");
+                j++;
+            }
+            System.out.println();
+            n++;
+        }
+
+        System.out.println();
+
+    }
+    //8.2
+    public static void drawInvertedStarsPiramid(int rows){
+        int g=rows;
+        while(g>=1){
+            int h =g;
+            while(h>=1){
+                System.out.print("* ");
+                h--;
+            }
+            System.out.println();
+            g--;
+        }
+
+        System.out.println();
+    }
+    //9.
+    public static void drawNumbersPiramid(int rows){
+        int r=1;
+        while(r<=rows){
+            int t =1;
+            while(t<=r){
+                System.out.print("* ");
+                t++;
+            }
+            System.out.println();
+            r++;
+        }
+
+        System.out.println();
+
+    }
+
+
+
+
+
+
+
 
 }
+
